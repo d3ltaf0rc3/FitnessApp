d7111ccde201a1d31686d3338a52ebd8707a5447
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2021-01-13 22:38:15 +0200

    set up jest

--- App.js
+++ App.js
@@ -16,38 +16,48 @@ const Stack = createStackNavigator();
 const App = () => {
   const [user, setUser] = useState(null);
 
-  const logIn = (user) => {
-    setUser(user);
+  const logIn = (respUser) => {
+    setUser(respUser);
   };
+
   const logOut = () => {
-    auth().signOut()
+    auth()
+      .signOut()
       .then(() => setUser(null))
       .catch((err) => console.error(err));
   };
 
   return (
-    <UserContext.Provider value={{
-      user,
-      logIn,
-      logOut
-    }}>
+    <UserContext.Provider
+      value={{
+        user,
+        logIn,
+        logOut,
+      }}>
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={context.user ? "Home" : "Login"}>
-          {user ?
+        <Stack.Navigator
+          screenOptions={{ headerShown: false }}
+          initialRouteName={context.user ? 'Home' : 'Login'}>
+          {user ? (
             <>
               <Stack.Screen name="Home" component={HomeScreen} />
               <Stack.Screen name="All workouts" component={AllWorkoutsScreen} />
-              <Stack.Screen name="Add a workout" component={AddAWorkoutScreen} />
+              <Stack.Screen
+                name="Add a workout"
+                component={AddAWorkoutScreen}
+              />
               <Stack.Screen name="View details" component={ViewDetailsScreen} />
-            </> :
-            <>
-              <Stack.Screen name="Login" component={LoginScreen} />
-              <Stack.Screen name="Register" component={RegisterScreen} />
-            </>}
+            </>
+          ) : (
+              <>
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Register" component={RegisterScreen} />
+              </>
+            )}
         </Stack.Navigator>
       </NavigationContainer>
     </UserContext.Provider>
-  )
+  );
 };
 
 export default App;
